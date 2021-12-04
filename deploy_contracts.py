56a22ac621492a6f1f2254d43d74623d97a97dc7
Tom <tom.marx@hotmail.fr>
2021-12-04 17:21:56 +0100

    Fixed deploy script

--- deploy_contracts.py
+++ deploy_contracts.py
@@ -6,11 +6,11 @@ print("Compiling the contracts...")
 os.system("cd backend && npx hardhat compile")
 
 factoryBuild = json.load(open("./backend/artifacts/contracts/Factory.sol/Factory.json"))
-projectBuild = json.load(open("./backend/artifacts/contracts/Factory.sol/Project.json"))
+projectBuild = json.load(open("./backend/artifacts/contracts/Project.sol/Project.json"))
 
 print("Copying new ABIs in the frontend...")
-json.dump("./frontend/src/assets/factory.json", open(factoryBuild['abi'], "w+"))
-json.dump("./frontend/src/assets/project.json", open(projectBuild['abi'], "w+"))
+json.dump(factoryBuild['abi'], open("./frontend/src/assets/factory.json", "w"))
+json.dump(projectBuild['abi'], open("./frontend/src/assets/project.json", "w"))
 
 print("Deploying factory contract...")
 req = requests.post(
@@ -27,4 +27,4 @@ req = requests.post(
 		'x-api-key': 'pk_13f910956993480fb87511cf5ee4a5a3'
 	}
 )
-print(f"Deployed at {req.json()['creationHash']}")
+print(f"Deployed at {req.json()['address']}")

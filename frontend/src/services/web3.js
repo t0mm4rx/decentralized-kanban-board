1f65be269f07096599389adb343514c331f901d9
Tom <tom.marx@hotmail.fr>
2021-12-04 12:59:05 +0100

    ABIs and getContracts functions

--- frontend/src/services/web3.js
+++ frontend/src/services/web3.js
@@ -1,4 +1,19 @@
 import { fakeProjects, fakeProject } from "./mockup"
+import * as config from "@/config.json"
+import * as factoryAbi from "@/assets/factory.json"
+import * as projectAbi from "@/assets/project.json"
+import { ethers } from "ethers";
+
+export const getFactoryContract = () => {
+	if (!window.factory) {
+		window.factory = new ethers.Contract(config.factoryAddress, factoryAbi);
+	}
+	return window.factory;
+};
+
+export const getProjectContract = (address) => {
+	return new ethers.Contract(address, projectAbi);
+};
 
 export const getProjects = () => {
 	return new Promise((resolve) => {
